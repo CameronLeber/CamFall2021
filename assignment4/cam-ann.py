ba8ce7ad577fcbb93fc17b3b737bb2150f0f64c6
CameronLeber <90150753+CameronLeber@users.noreply.github.com>
2021-11-17 12:35:01 -0800

    Update cam-ann.py

--- assignment4/cam-ann.py
+++ assignment4/cam-ann.py
@@ -27,8 +27,8 @@ class Node:
          print(f"{indent} {self_node_name}")
          return
                
-    print(f"{indent} {self.node_name} is connected to:")
-    for i in range(len(self.children) ):
+     print(f"{indent} {self.node_name} is connected to:")
+     for i in range(len(self.children) ):
             try: 
               print(f"{indent} Weight of {self.weight[i]}")
             except:
@@ -36,16 +36,16 @@ class Node:
             
             self.children[i].prety_print(current_layer_number + 1, node_per_layer_map)
             
-    return
-            
-   def set_random_weights(self, current_layer_number, node_per_layer_map):
-     if current_layer_number >= len(node_per_layer_map:
-       return
-     self.weight = [0.0] * len(self.children)
-     for i in range(len(self.children)):
-       self.weight[i] = random.uniform(0, 1)
-       self.children[i].set_random_weights(current_layer_number + 1, node_per_layer_map)
      return
+            
+    def set_random_weights(self, current_layer_number, node_per_layer_map):
+      if current_layer_number >= len(node_per_layer_map:
+        return
+      self.weight = [0.0] * len(self.children)
+      for i in range(len(self.children)):
+        self.weight[i] = random.uniform(0, 1)
+        self.children[i].set_random_weights(current_layer_number + 1, node_per_layer_map)
+      return
                                    
 new_node = Node()
                                                                  
